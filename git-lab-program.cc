c041dd7bf0c48991de73448bea8d63e8d57eeadc
NLAHER <nl927419@ohio.edu>
2023-09-05 18:58:17 -0400

    git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nick Laher
+ *        Date: 9/5/2023
+ * Description: GitHub tutorial Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
